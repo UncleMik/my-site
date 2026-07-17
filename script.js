6b4320177dec706eb02077df0b762125b9f6bf66
UncleMik <mikailkod.95@gmail.com>
2026-07-17 19:52:16 +0300

    Remove obsolete site files

--- script.js
+++
@@ -1,9 +0,0 @@
-// Только один открытый пункт FAQ одновременно
-document.querySelectorAll('.faq__item').forEach((item) => {
-  item.addEventListener('toggle', () => {
-    if (!item.open) return;
-    document.querySelectorAll('.faq__item').forEach((other) => {
-      if (other !== item) other.open = false;
-    });
-  });
-});
